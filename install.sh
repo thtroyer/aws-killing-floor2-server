02da93d07c7f63be1ae2c91253a0d6d7c3aadb96
Tom Troyer <tom.troyer@gmail.com>
2018-10-30 22:50:07 -0400

    Fixed updates.  Also auto runs script.

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ sudo sh -c 'echo "deb http://archive.ubuntu.com/ubuntu xenial-security main univ
 sudo dpkg --add-architecture i386
 
 # Do upgrades
-DEBIAN_FRONTEND=noninteractive
 sudo apt update
-#sudo apt upgrade -y
-sudo apt -y install debconf-utils htop ncdu unattended-upgrades
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install debconf-utils htop ncdu unattended-upgrades
 
 # Accept licence agreements
 # use debconf-get-selections to find them
@@ -21,14 +21,18 @@ echo steamcmd        steam/question  select  I AGREE | sudo debconf-set-selectio
 sudo apt -y install steamcmd
 
 # Create initial script
-mkdir /home/ubuntu/games/
-mkdir /home/ubuntu/games/kf2/
-touch /home/ubuntu/games/kf2/update.txt
-echo "login anonymous" >> /home/ubuntu/games/kf2/update.txt
-echo "force_install_dir /home/ubuntu/games/kf2" >> /home/ubuntu/games/kf2/update.txt
-echo "app_update 232130 validate" >> /home/ubuntu/games/kf2/update.txt
-echo "quit" >> /home/ubuntu/games/kf2/update.txt
+su ubuntu -c 'mkdir /home/ubuntu/games/'
+su ubuntu -c 'mkdir /home/ubuntu/games/kf2/'
+su ubuntu -c 'touch /home/ubuntu/games/kf2/update.txt'
+su ubuntu -c 'echo "login anonymous" >> /home/ubuntu/games/kf2/update.txt'
+su ubuntu -c 'echo "force_install_dir /home/ubuntu/games/kf2" >> /home/ubuntu/games/kf2/update.txt'
+su ubuntu -c 'echo "app_update 232130 validate" >> /home/ubuntu/games/kf2/update.txt'
+su ubuntu -c 'echo "quit" >> /home/ubuntu/games/kf2/update.txt'
 
 # Install KF2
-steamcmd +runscript /home/ubuntu/games/kf2/update.txt
+su ubuntu -c 'steamcmd +runscript /home/ubuntu/games/kf2/update.txt'
 
+# Create run script
+su ubuntu -c 'echo "/home/ubuntu/games/kf2/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab" > /home/ubuntu/runKf2.sh'
+su ubuntu -c 'chmod +x /home/ubuntu/runKf2.sh'
+nohup su ubuntu -c 'nohup /home/ubuntu/runKf2.sh'
